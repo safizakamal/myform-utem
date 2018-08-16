0677f5351c2c388a8eeb5bad9b887069fd51f4c2
unknown <safiza@email.com>
2018-08-16 09:19:58 +0800

    add menu

--- app/src/main/java/my/edu/utem/myform/WebActivity.java
+++ app/src/main/java/my/edu/utem/myform/WebActivity.java
@@ -2,13 +2,22 @@ package my.edu.utem.myform;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebActivity extends AppCompatActivity {
+
+    WebView webView; //untuk panggil web page
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_web);
+
+        webView = findViewById(R.id.webView); //bukak di current app in app browser
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true); //setting guna javascript
+        webView.loadUrl("http://www.utem.edu.my");
     }
 }
